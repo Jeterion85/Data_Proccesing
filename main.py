b690a38052dfe8dd39f557bd7823c066d7a7f81d
Jeterion85 <ggakis155@gmail.com>
2022-01-04 18:09:27 +0200

    Error on main dataset Inner node calls compact

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from DBMS import *
 import os
 import datetime
 import math
-
+import time
 
 for file in os.listdir('./Data_Blocks/'):
     os.remove('Data_Blocks/'+file)
@@ -17,7 +17,7 @@ x_low=-10.0
 y_low=45.0
 width=10
 height=6
-sector_size=input('Please enter the sector size(default:512 bytes):')
+sector_size=512#input('Please enter the sector size(default:512 bytes):')
 if sector_size=='':
     sector_size=512
 else:
@@ -25,10 +25,10 @@ else:
 max_leaf_size=math.floor(sector_size/47)
 Quad_Tree(x_low,y_low,width,height,max_leaf_size)
 Hash_Index()
-x=float(input('Please enter the Longitude of the accident(-10:0):'))
-y=float(input('Plese enter the Latitude of the accident(45,51):'))
-signal_range=float(input('Please enter the signal range(unit:km):'))
-accident_date=input('Plese enter the the date of the accident(d-m-Y H:M:S):').strip()
+x=-5#float(input('Please enter the Longitude of the accident(-10:0):'))
+y=48#float(input('Plese enter the Latitude of the accident(45,51):'))
+signal_range=100#float(input('Please enter the signal range(unit:km):'))
+accident_date='30-10-2015 22:30:30'#input('Plese enter the the date of the accident(d-m-Y H:M:S):').strip()
 a_day=int(accident_date.split(' ')[0].split('-')[0])
 a_month=int(accident_date.split(' ')[0].split('-')[1])
 a_year=int(accident_date.split(' ')[0].split('-')[2])
@@ -36,9 +36,8 @@ a_hour=int(accident_date.split(' ')[1].split(':')[0])
 a_minute=int(accident_date.split(' ')[1].split(':')[1])
 a_second=int(accident_date.split(' ')[1].split(':')[2])
 accident_date=datetime.datetime(day=a_day,month=a_month,year=a_year,hour=a_hour,minute=a_minute,second=a_second)
-for line in open('./Test_data/Test.csv','r').readlines():
+for line in open('./Data/nari_dynamic.csv','r'):
     line=line.strip()
-    print(line)
     date=line.split(',')[3]
     day=int(date.split(' ')[0].split('-')[0])
     month=int(date.split(' ')[0].split('-')[1])
@@ -48,6 +47,7 @@ for line in open('./Test_data/Test.csv','r').readlines():
     second=int(date.split(' ')[1].split(':')[2])
     current_date=datetime.datetime(day=day,month=month,year=year,hour=hour,minute=minute,second=second)
     if current_date>accident_date:
+        start_time=time.time()
         line=line.strip()
         Quad_Tree.insert(line.split(',')[0],float(line.split(',')[1]),float(line.split(',')[2]),line.split(',')[3])      
         x_r_MBR=x-360*signal_range*10**3/40075000
@@ -61,3 +61,5 @@ for line in open('./Test_data/Test.csv','r').readlines():
         Quad_Tree.insert(line.split(',')[0],float(line.split(',')[1]),float(line.split(',')[2]),line.split(',')[3])
 for result in Quad_Tree.range_query_results:
     print(result.strip())
+end_time=time.time()
+print(f'TOTAL TIME:{end_time-start_time} sec')
--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,11 @@
 from datetime import datetime
-data=open('C:/Users/George/Documents/[P1] AIS Data/nari_dynamic.csv','r')
+data=open('/home/george/Documents/nari_dynamic.csv','r')
 data_1=open('./Data/nari_dynamic.csv','w')
-for line in data.readlines()[1:]:
+i=True
+for line in data:
+    if i:
+        i=not i
+        continue
     line=line.strip()
     date=datetime.utcfromtimestamp(int(line.split(',')[8])).strftime('%d-%m-%Y %H:%M:%S')
     data_1.write(line.split(',')[0]+','+line.split(',')[6]+','+line.split(',')[7]+','+date+'\n')
